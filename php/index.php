3dda7c5ab3bbb0f56872caacec71720027f864bc
nikita-chaurasiya <nikita.chaurasiya@gmail.com>
2024-10-15 12:54:37 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "172.17.0.2";
+$servername = "database";
 $username = "root";
 $password = "123";
 $dbname = "mydb";
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT firstname, rollnumber FROM tb";
+$sql = "SELECT firstname, rollnumber FROM natwest";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
