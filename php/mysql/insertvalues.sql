b6f575a1d580d54c09f27ded5ca9ee4e7760ceb7
nikita-chaurasiya <nikita.chaurasiya@gmail.com>
2024-10-15 12:43:16 +0530

    Delete php/mysql directory

--- php/mysql/insertvalues.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tb (firstname, rollnumber) VALUES ('Ali',1);
-INSERT INTO tb (firstname, rollnumber) VALUES ('Jaimie',2);
-INSERT INTO tb (firstname, rollnumber) VALUES ('Kalyan',3);
-INSERT INTO tb (firstname, rollnumber) VALUES ('Nikita',4);
-INSERT INTO tb (firstname, rollnumber) VALUES ('Veera',5);
