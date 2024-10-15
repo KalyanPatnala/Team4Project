d020bc8b95f5fb22da02aedaa2bb68f05c6b2b6d
nikita-chaurasiya <nikita.chaurasiya@gmail.com>
2024-10-15 12:42:54 +0530

    Create insertvalues.sql

---
+++ php/mysql/insertvalues.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb (firstname, rollnumber) VALUES ('Ali',1);
+INSERT INTO tb (firstname, rollnumber) VALUES ('Jaimie',2);
+INSERT INTO tb (firstname, rollnumber) VALUES ('Kalyan',3);
+INSERT INTO tb (firstname, rollnumber) VALUES ('Nikita',4);
+INSERT INTO tb (firstname, rollnumber) VALUES ('Veera',5);
