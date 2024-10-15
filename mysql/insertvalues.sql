175e9344eaeff6f21bc76dd91408dfc4ba59ae14
nikita-chaurasiya <nikita.chaurasiya@gmail.com>
2024-10-15 12:45:14 +0530

    Create insertvalues.sql

---
+++ mysql/insertvalues.sql
@@ -0,0 +1,5 @@
+INSERT INTO natwest (firstname, rollnumber) VALUES ('Ali',1);
+INSERT INTO natwest (firstname, rollnumber) VALUES ('Jamie',2);
+INSERT INTO natwest (firstname, rollnumber) VALUES ('Kalyan',3);
+INSERT INTO natwest (firstname, rollnumber) VALUES ('Nikita',4);
+INSERT INTO natwest (firstname, rollnumber) VALUES ('Veera',5);
